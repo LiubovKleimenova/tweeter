0bc852b58e27ca98eca9978c468b8bf11070de5d
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-25 16:07:30 -0500

    added tweet markup and styles

---
+++ public/scripts/counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  
+  $("textarea").keyup( function() {
+    
+    let $remL = 140 - $(this).val().length;
+    console.log($remL);
+    $(this).siblings().text($remL);
+    if ($remL < 0) {
+      $(this).siblings(".counter").addClass("red-font");
+     } else {
+       $(this)
+         .siblings(".counter")
+         .removeClass("red-font");
+     }
+  })
+});
