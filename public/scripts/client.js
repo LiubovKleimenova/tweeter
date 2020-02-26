a87aa0f5c9112785e95ecbd535af9761fa1dab76
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-25 21:56:03 -0500

    added classes for specificity

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- $(document).ready(function() {
+$(document).ready(function() {
+  const SECONDS = 86400000; //24*60*60*1000 number of milliseconds in 1 day
   const data = [
     {
       user: {
@@ -33,7 +34,7 @@
 
   const renderTweets = function(tweets) {
     for (let tweet of tweets) {
-      $(".tweet-container").append(createTweetElement(tweet))
+      $(".tweet-container").append(createTweetElement(tweet));
     }
   };
 
@@ -41,28 +42,33 @@
     //let {name, avatar, handle} = tweet.user;
     let tweetContent = tweet.content.text;
     let tweetDate = tweet.created_at;
-    let $tweet = `<article>
-          <header>
+    let $tweet = `<article class="tweet">
+          <header class="tweet-header">
             <div>
-              <img class="userphoto" src="${tweet.user.avatars}" alt="user photo" width="70" height="70">
+              <img class="userphoto" src="${
+                tweet.user.avatars
+              }" alt="user photo" width="70" height="70">
               <span>${tweet.user.name}</span>
             </div>
             <p class="username">${tweet.user.handle}</p>
             
           </header>
        
-          <main>
+          <main class="tweet-main">
             ${tweetContent}
           </main>
-          <footer>
-            <p>${Math.round((Date.now()-tweetDate)/86400000)} days ago</p>
+          <footer class="tweet-footer">
+            <p>${Math.round((Date.now() - tweetDate) / SECONDS)} days ago</p>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
           </footer>
         </article>`;
 
-    
     return $tweet;
   };
 
   renderTweets(data);
- }) 
-
+});
