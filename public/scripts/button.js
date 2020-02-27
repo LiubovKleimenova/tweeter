cafb518f575ddabf81f86a6f3078c82f5362a952
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-27 14:42:55 -0500

    added responsive design

--- public/scripts/button.js
+++ public/scripts/button.js
@@ -1,18 +1,6 @@
-
-
 $(document).ready(function() {
-  
-  let position = 0;
-
-  $(".nav-button").click(function() {
-    if (!position) {
-    $(".new-tweet").addClass("up");
-    $(".new-tweet textarea").focus();
-    position = 1
-    } else {
-      $(".new-tweet").removeClass("up");
-      position = 0;
-    }
-    
-})
+  $(".nav-button").click(function() {    
+      $(".new-tweet").slideToggle("slow");
+      $(".new-tweet textarea").focus();
+  })
 })
