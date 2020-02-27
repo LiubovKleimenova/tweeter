c190071f83c2e9c6a53bcb767daaa8da54c79663
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-27 16:52:39 -0500

    added scroll logic

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -5,10 +5,15 @@ $(document).ready(function() {
     if ($(document).scrollTop() === 0) {
       $(".scroll-button").removeClass("scroll-on");
     } 
-    if ($(document).scrollTop() > 400) {
+    if ($(document).scrollTop() > 200) {
       $(".nav-button").addClass("btn-off");
     } else {
       $(".nav-button").removeClass("btn-off");
     }
-})
+  })
+      $(".scroll-button").on('click', function() {
+        $(document).scrollTop(0,0);
+        $(".new-tweet").slideToggle("slow");
+        $(".new-tweet textarea").focus();
+      })
 })
